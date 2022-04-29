61b9303d3cf316b4d5d8974b415beb7d9b0b964f
lilianux <lilianpierredamiens@gmail.com>
2022-04-29 17:15:20 +0200

    [IMP] everest: removing old test folder

--- bin/launcher.php
+++ bin/launcher.php
@@ -7,6 +7,5 @@ require_once(__DIR__ . '/../src/libs/autoloader.php');
 $tester = Tester::instance();
 
 $tester->directory("EverestTests", __DIR__ . "/../tests")
-		->directory("EverestTests2", __DIR__ . "/../test2")
 		->run();
 ?>
