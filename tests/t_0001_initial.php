2df5fdb4a621c7ffcb113bcd583f7a74b069a8d6
lilianux <lilianpierredamiens@gmail.com>
2022-04-30 02:07:25 +0200

    [FIX] Add a test attribute to t_0001

--- tests/t_0001_initial.php
+++ tests/t_0001_initial.php
@@ -4,38 +4,42 @@ use Everest\core\entities\TestCase;
 
 class t_0001_initial extends TestCase
 {
-	public string $testAttribute = "";
+	private string $testAttribute;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
 	public function TestKeyPass()
 	{
-		return($this->assertArrayHasKey(3, [1, 2, 3]));
+		$this->assertArrayHasKey(3, [1, 2, 3]);
 	}
 
 	public function TestKeyFail()
 	{
-		return($this->assertArrayHasKey(3, [1, 2, 4]));
+		$this->assertArrayHasKey(3, [1, 2, 4]);
 	}
 
 	public function TestClassPropertyPass()
 	{
-		return ($this->assertClassHasProperty("testAttribute", self::class));
+		$this->assertClassHasProperty("testAttribute", self::class);
+		$this->assertClassHasProperty("testAttribute", self::class);
 	}
 
 	public function TestClassPropertyFail()
 	{
-		return ($this->assertClassHasProperty("random", self::class));
+		$this->assertClassHasProperty("random", self::class);
 	}
 
 	public function TestArraySubsetPass()
 	{
-		return ($this->assertArraySubset(['test' => [1, 2]], ['test' => [1, 2, 3]], "pute"));
+		$this->assertArraySubset(['test' => [1, 2]], ['test' => [1, 2, 3]], "pute");
 	}
 
 	public function TestArraySubsetFail()
 	{
-		return ($this->assertArraySubset(['test' => [1, 4]], ['test' => [1, 2, 3]]));
+		$this->assertArraySubset(['test' => [1, 4]], ['test' => [1, 2, 3]]);
 	}
-
-
 }
 ?>
