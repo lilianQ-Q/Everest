b678c6b793a0fa53edcf7891580b0ab20f42b813
lilianux <lilianpierredamiens@gmail.com>
2022-04-30 02:03:22 +0200

    [IMP] everest: Refacto + Display with colors different tests

--- src/core/entities/TestCase.php
+++ src/core/entities/TestCase.php
@@ -1,11 +1,20 @@
 <?php
 namespace Everest\core\entities;
 
+use Everest\core\exception\TestFailException;
+use Everest\Tester;
 use ReflectionClass;
 
 // https://phpunit.readthedocs.io/fr/latest/assertions.html#assertarrayhaskey
 abstract class TestCase
 {
+
+	protected Tester $tester;
+
+	protected function __construct()
+	{
+		$this->tester = Tester::instance();
+	}
 
 	/**
 	 * Test if a needle value is present in a haystack array
@@ -15,11 +24,11 @@ abstract class TestCase
 	 * @param string $message
 	 * @return TestResult
 	 */
-	protected function assertArrayHasKey(mixed $value, array $array, string $message = '') : TestResult
+	protected function assertArrayHasKey(mixed $value, array $array, string $message = '') : void
 	{
-		if (in_array($value, $array))
-			return (TestResult::pass("The array contains '$value' !"));
-		return (TestResult::fail(empty($message) ? "The array does not contains value '\e[1m$value\e[0m'." : $message));
+		if (!in_array($value, $array))
+			throw new TestFailException(empty($message) ? "They array doest not contains value $value" : $message);
+		$this->tester->addAssertion();
 	}
 
 	/**
@@ -29,37 +38,32 @@ abstract class TestCase
 	 * @param string $className
 	 * @param string $message
 	 */
-	protected function assertClassHasProperty(string $attribute, string $className, string $message = '') : TestResult
+	protected function assertClassHasProperty(string $attribute, string $className, string $message = '') : void
 	{
 		$rc = new ReflectionClass($className);
-		if (!empty($rc->hasProperty($attribute)))
-			return (TestResult::pass("The '$className' class contains a '$attribute' attribute !"));
-		return (TestResult::fail(empty($message) ? "The '\e[1m$className\e[0m' class doest not contains a '\e[1m$attribute\e[0m' attribute." : $message));
+		if (empty($rc->hasProperty($attribute)))
+			throw new TestFailException("The \"$className\e class does not contains a \"$attribute\" attribute");
+		$this->tester->addAssertion();
 	}
 
 	protected function assertArraySubset(array $subset, array $haystack, string $message = '')
 	{
 		if (!(count($subset) != 0 && count($haystack) != 0))
-			return TestResult::fail($message);
+			throw new TestFailException($message);
+
 		foreach ($subset as $key => $value)
 		{
-			if (is_array($value))
-			{
-				if (array_key_exists($key, $haystack))
-				{
-					foreach($value as $vvalue)
-					{
-						if (!in_array($vvalue, $haystack[$key]))
-							return (TestResult::fail("ewew"));
-					}
-				}
-				else
-					return (TestResult::fail($message));
-			}
-			else
-				return (TestResult::fail($message));
+			if (!is_array($value))
+				throw new TestFailException($message);
+
+			if (!array_key_exists($key, $haystack))
+				throw new TestFailException($message);
+
+			foreach ($value as $vvalue)
+				if (!in_array($vvalue, $haystack[$key]))
+					throw new TestFailException($message);
 		}
-		return (TestResult::pass("yay"));
+		$this->tester->addAssertion();
 	}
 }
 
