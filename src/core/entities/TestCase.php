cad397cf08893cabe421e3b589cda192c1412eae
lilianux <lilianpierredamiens@gmail.com>
2022-05-01 17:43:27 +0200

    [IMP] everest: Adds a whole new bunch of tests including the famous Equals one

--- src/core/entities/TestCase.php
+++ src/core/entities/TestCase.php
@@ -3,7 +3,9 @@ namespace Everest\core\entities;
 
 use Everest\core\exception\TestFailException;
 use Everest\Tester;
+use Exception;
 use ReflectionClass;
+use ReflectionNamedType;
 
 // https://phpunit.readthedocs.io/fr/latest/assertions.html#assertarrayhaskey
 abstract class TestCase
@@ -31,6 +33,13 @@ abstract class TestCase
 		$this->tester->addAssertion();
 	}
 
+	protected function assertNotArrayHasKey(mixed $value, array $array, string $message = '') : void
+	{
+		if (in_array($value, $array))
+			throw new TestFailException(empty($message) ? "They array contains value $value" : $message);
+		$this->tester->addAssertion();
+	}
+
 	/**
 	 * Test if a class contains a specified property
 	 * 
@@ -43,6 +52,14 @@ abstract class TestCase
 		$rc = new ReflectionClass($className);
 		if (empty($rc->hasProperty($attribute)))
 			throw new TestFailException("The \"$className\e class does not contains a \"$attribute\" attribute");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotClassHasProperty(string $attribute, string $className, string $message = '') : void
+	{
+		$rc = new ReflectionClass($className);
+		if ($rc->hasProperty($attribute))
+			throw new TestFailException("The \"$className\e class contains a \"$attribute\" attribute");
 		$this->tester->addAssertion();
 	}
 
@@ -63,6 +80,350 @@ abstract class TestCase
 				if (!in_array($vvalue, $haystack[$key]))
 					throw new TestFailException($message);
 		}
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotArraySubset(array $subset, array $haystack, string $message = '')
+	{
+		if (!(count($subset) != 0 && count($haystack) != 0))
+			throw new TestFailException($message);
+
+		foreach ($subset as $key => $value)
+		{
+			if (!is_array($value))
+				throw new TestFailException($message);
+
+			if (!array_key_exists($key, $haystack))
+				throw new TestFailException($message);
+
+			foreach ($value as $vvalue)
+				if (in_array($vvalue, $haystack[$key]))
+					throw new TestFailException($message);
+		}
+		$this->tester->addAssertion();
+	}
+
+	protected function assertClassHasStaticAttribute(string $attributeName, string $className, string $message = '')
+	{
+		$rp = new \ReflectionProperty($className, $attributeName);
+		if (!($rp && $rp->isStatic()))
+			throw new TestFailException($message ? $message : "The class does not contains a $attributeName static attribute");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotClassHasStaticAttribute(string $attributeName, string $className, string $message = '')
+	{
+		try
+		{
+			$rp = new \ReflectionProperty($className, $attributeName);
+		}
+		catch (\Exception $exception)
+		{
+			$this->tester->addAssertion();
+			return ;
+		}
+		if (($rp && $rp->isStatic()))
+			throw new TestFailException($message ? $message : "The class contains a $attributeName static attribute");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertContains(mixed $needle, array $haystack, string $message = '')
+	{
+		if (!in_array($needle, $haystack))
+			throw new TestFailException($message ? $message : "They haystack does not contains the needle value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotContains(mixed $needle, array $haystack, string $message = '')
+	{
+		if (in_array($needle, $haystack))
+			throw new TestFailException($message ? $message : "They haystack contains the needle value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertAttributeContains(mixed $needle, object $instance, string $attribute, string $message = '')
+	{
+		$rc = new ReflectionClass($instance);
+		foreach ($rc->getProperties() as $property)
+		{
+			if ($property->getName() === $attribute)
+			{
+				$property->setAccessible(1);
+				if ($property->getType()->getName() !== "array")
+					throw new Exception("The $attribute property must be an array");
+				if (!in_array($needle, $property->getValue($instance)))
+					throw new Exception("The $attribute property does not contains needle value");
+				$this->tester->addAssertion();
+				return ;
+			}
+		}
+		throw new Exception("The " . $instance::class . "class does not contains $attribute attribute");	
+	}
+
+	protected function assertNotAttributeContains(mixed $needle, object $instance, string $attribute, string $message = '')
+	{
+		$rc = new ReflectionClass($instance);
+		foreach ($rc->getProperties() as $property)
+		{
+			if ($property->getName() === $attribute)
+			{
+				$property->setAccessible(1);
+				if ($property->getType()->getName() !== "array")
+					throw new Exception("The $attribute property must be an array");
+				if (in_array($needle, $property->getValue($instance)))
+					throw new Exception("The $attribute property contains needle value");
+				$this->tester->addAssertion();
+				return ;
+			}
+		}
+		throw new Exception("The " . $instance::class . "class does not contains $attribute attribute");
+	}
+
+	//TODO: use isNative in the function
+	protected function assertContainsOnly(string $type, array $haystack, bool $isNative = null, string $message = '')
+	{
+		foreach ($haystack as $element)
+		{
+			if (gettype($element) !== $type)
+				throw new Exception("One element is not a $type type");
+		}
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotContainsOnly(string $type, array $haystack, bool $isNative = null, string $message = '')
+	{
+		$typeCount = 0;
+		foreach ($haystack as $element)
+		{
+			if (gettype($element) === $type)
+				$typeCount++;
+		}
+		if ($typeCount === count($haystack))
+			throw new Exception("They array contains only $type values");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertCount(int $expectedCount, array $haystack, string $message = '')
+	{
+		if ($expectedCount !== count($haystack))
+			throw new Exception($message ? $message : "Haystack count must be equal to $expectedCount");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotCount(int $expectedCount, array $haystack, string $message = '')
+	{
+		if ($expectedCount === count($haystack))
+			throw new Exception($message ? $message : "Haystack count is equal to $expectedCount");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertDirectoryExists(string $directory, string $message = '')
+	{
+		if (!is_dir($directory))
+		{
+			if (!is_file($directory))
+				throw new Exception("Specified directory $directory does not exists");
+			else
+				throw new Exception("Specified directory $directory is a file");
+		}
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotDirectoryExists(string $directory, string $message = '')
+	{
+		if (is_dir($directory))
+			throw new Exception($message ? $message : "Specified directory IS a directory (it must not)");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertDirectoryReadable(string $directory, string $message = '')
+	{
+		if (!is_readable($directory))
+			throw new Exception($message ? $message : "Specified directory must be readable");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotDirectoryReadable(string $directory, string $message = '')
+	{
+		if (is_readable($directory))
+			throw new Exception($message ? $message : "Specified directory is readable");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertDirectoryWriteable(string $directory, string $message = '')
+	{
+		if (!is_writeable($directory))
+			throw new Exception($message ? $message : "Specified directory must be writeable");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotDirectoryWriteable(string $directory, string $message = '')
+	{
+		if (is_writeable($directory))
+			throw new Exception($message ? $message : "Specified directory is writeable");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertEmpty(mixed $value, string $message = '')
+	{
+		if (!empty($value))
+			throw new Exception($message ? $message : "Value must be empty");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotEmpty(mixed $value, string $message = '')
+	{
+		if (empty($value))
+			throw new Exception($message ? $message : "Value is empty");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertEquals(mixed $expected, mixed $value, string $message = '')
+	{
+		if ($expected !== $value)
+			throw new Exception($message ? $message : "Expected value must be equal to \$value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotEquals(mixed $expected, mixed $value, string $message = '')
+	{
+		if ($expected === $value)
+			throw new Exception($message ? $message : "Expected value is equal to \$value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertFalse(bool $condition, string $message = '')
+	{
+		if ($condition)
+			throw new Exception($message ? $message : "Condition must be false");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotFalse(bool $condition, string $message = '')
+	{
+		if (!$condition)
+			throw new Exception($message ? $message : "Condition is false");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertFileExists(string $filename, string $message = '')
+	{
+		if (!file_exists($filename))
+			throw new Exception($message ? $message : "Filename must exists");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotFileExists(string $filename, string $message = '')
+	{
+		if (file_exists($filename))
+			throw new Exception($message ? $message : "Filename exists");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertGreaterThan(mixed $expected, mixed $actual, string $message = '')
+	{
+		if (!($actual > $expected))
+			throw new Exception($message ? $message : "\$actual value must be greater than expected value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotGreaterThan(mixed $expected, mixed $actual, string $message = '')
+	{
+		if ($actual > $expected)
+			throw new Exception($message ? $message : "\$actual is greater than expected value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertGreaterThanOrEqual(mixed $expected, mixed $actual, string $message = '')
+	{
+		if (!($actual >= $expected))
+			throw new Exception($message ? $message : "\$actual value must be greater or equal to expected");
+		$this->tester->addAssertion();	
+	}
+
+	protected function assertInstanceOf(object $expected, string $actual, string $message = '')
+	{
+		if (!($expected instanceof $actual))
+			throw new Exception($message ? $message : "Expected value must be an instance of $actual");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotInstanceOf(object $expected, string $actual, string $message = '')
+	{
+		if ($expected instanceof $actual)
+			throw new Exception($message ? $message : "Expected value is instance of actual $actual");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertLessThan(mixed $expected, mixed $actual, string $message = '')
+	{
+		if (!($actual < $expected))
+			throw new Exception($message ? $message : "Actual value must be less than expected value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertLessThanOrEqual(mixed $expected, mixed $actual, string $message = '')
+	{
+		if (!($actual <= $expected))
+			throw new Exception($message ? $message : "Actual value must be less or equal to expected");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNull(mixed $variable, string $message = '')
+	{
+		if (!is_null($variable))
+			throw new Exception($message ? $message : "The given variable must be null");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotNull(mixed $variable, string $message = '')
+	{
+		if (is_null($variable))
+			throw new Exception($message ? $message : "The given variable is null");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertTrue(mixed $condition, string $message = '')
+	{
+		if (!$condition)
+			throw new Exception($message ? $message : "The given condition must be true");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotTrue(mixed $condition, string $message = '')
+	{
+		if ($condition)
+			throw new Exception($message ? $message : "The given condition is true");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertSame(mixed $expected, mixed $actual, string $message = '')
+	{
+		if (gettype($actual) !== gettype($expected))
+			throw new Exception("Both values must be the same type");
+		if ($actual !== $expected)
+			throw new Exception($message ? $message : "Both values must be equals");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertNotSame(mixed $expected, mixed $actual, string $message = '')
+	{
+		if ((gettype($actual) === gettype($expected)) || ($actual === $expected))
+			throw new Exception($message ? $message : "Both values have same types or values");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertStringStartsWith(string $needle, string $haystack, string $message = '')
+	{
+		if (strpos($haystack, $needle) !== 0)
+			throw new Exception($message ? $message : "Haystack string must begin with needle value");
+		$this->tester->addAssertion();
+	}
+
+	protected function assertStringStartsNotWith(string $needle, string $haystack, string $message = '')
+	{
+		if (strpos($haystack, $needle) === 0)
+			throw new Exception($message ? $message : "Haystack starts with needle value");
 		$this->tester->addAssertion();
 	}
 }
--- tests/t_0001_initial.php
+++ tests/t_0001_initial.php
@@ -1,45 +1,287 @@
 <?php
 namespace EverestTests;
 use Everest\core\entities\TestCase;
+use Everest\core\entities\TestResult;
+use Everest\core\exception\TestFailException;
+use Everest\Tester;
 
 class t_0001_initial extends TestCase
 {
 	private string $testAttribute;
+
+	private static string $hello;
+
+	private array $arraytest = ["hey", "this is", "test"];
 
 	public function __construct()
 	{
 		parent::__construct();
 	}
 
-	public function TestKeyPass()
+	public function TestArrayHasKey()
 	{
 		$this->assertArrayHasKey(3, [1, 2, 3]);
 	}
 
-	public function TestKeyFail()
+	public function TestArrayHasNotKey()
 	{
-		$this->assertArrayHasKey(3, [1, 2, 4]);
+		$this->assertNotArrayHasKey(4, [1, 2, 3]);
 	}
 
-	public function TestClassPropertyPass()
+	public function TestClassHasProperty()
 	{
 		$this->assertClassHasProperty("testAttribute", self::class);
-		$this->assertClassHasProperty("testAttribute", self::class);
 	}
 
-	public function TestClassPropertyFail()
+	public function TestClassHasNotProperty()
 	{
-		$this->assertClassHasProperty("random", self::class);
+		$this->assertNotClassHasProperty("boogi", self::class);
 	}
 
-	public function TestArraySubsetPass()
+	public function TestArraySubset()
 	{
 		$this->assertArraySubset(['test' => [1, 2]], ['test' => [1, 2, 3]], "pute");
 	}
 
-	public function TestArraySubsetFail()
+	public function TestNotArraySubset()
 	{
-		$this->assertArraySubset(['test' => [1, 4]], ['test' => [1, 2, 3]]);
+		$this->assertNotArraySubset(['test' => [1, 2]], ['test' => [4, 5, 6]], "pute");
+	}
+
+	public function TestClassHasStaticAttribute()
+	{
+		$this->assertClassHasStaticAttribute("hello", self::class);
+	}
+
+	public function TestNotClassHasStaticAttribute()
+	{
+		$this->assertNotClassHasStaticAttribute("dontexist", self::class);
+	}
+
+	public function TestContains()
+	{
+		$this->assertContains("needle", ["blah", 21, "Blih", "needle", 42]);
+	}
+
+	public function TestNotContains()
+	{
+		$this->assertNotContains("needle", ["blah", 21, "Blih", 42]);
+	}
+
+	public function TestAttributeContains()
+	{
+		$test = new t_0001_initial();
+		$this->assertAttributeContains("test", $test, "arraytest");
+	}
+
+	public function TestNotAttributeContains()
+	{
+		$test = new t_0001_initial();
+		$this->assertNotAttributeContains("poom", $test, "arraytest");
+	}
+
+	public function TestContainsOnly()
+	{
+		$tmp = [1, 45, 3];
+		$this->assertContainsOnly("integer", $tmp);
+	}
+
+	public function TestNotContainsOnly()
+	{
+		$tmp = [1, 'ez', 3];
+		$this->assertNotContainsOnly("integer", $tmp);
+	}
+
+	public function TestCount()
+	{
+		$tmp = ["one", "two", "three"];
+		$this->assertCount(3, $tmp);
+	}
+
+	public function TestNotCount()
+	{
+		$tmp = ["four", "five", "six"];
+		$this->assertNotCount(4, $tmp);
+	}
+
+	public function TestDirectoryExists()
+	{
+		$this->assertDirectoryExists(__DIR__ . "/../src");
+	}
+
+	public function TestNotDirectoryExists()
+	{
+		$this->assertNotDirectoryExists(__DIR__ . "/../useless");
+	}
+
+	public function TestDirectoryReadable()
+	{
+		$this->assertDirectoryReadable(__DIR__ . "/../src");
+	}
+
+	public function TestNotDirectoryReadable()
+	{
+		$this->assertNotDirectoryReadable(__DIR__ . "/../useless");
+	}
+
+	public function TestDirectoryWriteable()
+	{
+		$this->assertDirectoryWriteable(__DIR__ . "/../src");
+	}
+
+	public function TestNotDirectoryWriteable()
+	{
+		$this->assertNotDirectoryWriteable(__DIR__ . "/../useless");
+	}
+
+	public function TestAssertEmpty()
+	{
+		$array = [];
+		$string = '';
+		$int = 0;
+		$null = null;
+		$this->assertEmpty($array);
+		$this->assertEmpty($string);
+		$this->assertEmpty($int);
+		$this->assertEmpty($null);
+	}
+
+	public function TestAssertNotEmpty()
+	{
+		$array = ['ezez'];
+		$string = 'ezez';
+		$int = 1;
+		$null = true;
+		$this->assertNotEmpty($array);
+		$this->assertNotEmpty($string);
+		$this->assertNotEmpty($int);
+		$this->assertNotEmpty($null);
+	}
+
+	public function TestAssertEquals()
+	{
+		$this->assertEquals([1, 2], [1, 2]);
+		$this->assertEquals(null, null);
+		$this->assertEquals(1, 1);
+		$this->assertEquals("Pouet", "Pouet");
+	}
+
+	public function TestAssertNotEquals()
+	{
+		$this->assertNotEquals([1, 3], [1, 2]);
+		$this->assertNotEquals(null, true);
+		$this->assertNotEquals(1, 2);
+		$this->assertNotEquals("Pouet", "Pouetyo");
+	}
+
+	public function TestAssertFalse()
+	{
+		$this->assertFalse(1 === 2);
+		$this->assertFalse([1, 1] === [1, 2]);
+		$this->assertFalse("pouet" === "ah!");
+	}
+
+	public function TestAssertNotFalse()
+	{
+		$this->assertNotFalse(1 === 1);
+		$this->assertNotFalse([1, 1] === [1, 1]);
+		$this->assertNotFalse("pouet" === "pouet");
+	}
+
+	public function TestAssertFileExists()
+	{
+		$this->assertFileExists(__DIR__ . "/../config/autoloader.json");
+	}
+
+	public function TestAssertNotFileExists()
+	{
+		$this->assertNotFileExists(__DIR__ . "/../config/ptdr.json");
+	}
+
+	public function TestAssertGreaterThan()
+	{
+		$this->assertGreaterThan(1, 56);
+	}
+
+	public function TestAssertNotGreaterThan()
+	{
+		$this->assertNotGreaterThan(56, 1);
+	}
+
+	public function TestAssertGreaterThanOrEqual()
+	{
+		$this->assertGreaterThanOrEqual(56, 56);
+		$this->assertGreaterThanOrEqual(56, 100);
+	}
+
+	public function TestAssertInstanceOf()
+	{
+		$test = new t_0001_initial();
+		$this->assertInstanceOf($test, self::class);
+	}
+
+	public function TestAssertNotInstanceOf()
+	{
+		$test = new t_0001_initial();
+		$this->assertNotInstanceOf($test, "Everest\\zebi");
+	}
+
+	public function TestAssertLessThan()
+	{
+		$this->assertLessThan(50, 40);
+	}
+
+	public function TestAssertLessThanOrEqual()
+	{
+		$this->assertLessThanOrEqual(50, 50);
+	}
+
+	public function TestAssertNull()
+	{
+		$this->assertNull(null);
+	}
+
+	public function TestAssertNotNull()
+	{
+		$this->assertNotNull("null");
+	}
+	
+	public function TestAssertTrue()
+	{
+		$this->assertTrue(1 === 1);
+		$this->assertTrue([1, 1] === [1, 1]);
+		$this->assertTrue("string" === "string");
+	}
+
+	public function TestNotAssertTrue()
+	{
+		$this->assertNotTrue(1 === 2);
+		$this->assertNotTrue([1, 1] === [1, 2]);
+		$this->assertNotTrue("string" === "slipdebain");
+	}
+
+	public function TestAssertSame()
+	{
+		$instance = new TestFailException("ez");
+		$this->assertSame($instance, $instance);
+		$this->assertSame("oui", "oui");
+	}
+
+	public function TestAssertNotSame()
+	{
+		$instance = new TestFailException("no");
+		$this->assertNotSame($instance, "\$instance");
+		$this->assertNotSame("oui", 1);
+	}
+
+	public function TestAssertStringStartsWith()
+	{
+		$this->assertStringStartsWith("Salut, ", "Salut, jespere que tu vas bien.");
+	}
+
+	public function TestAssertStringStartsNotWith()
+	{
+		$this->assertStringStartsNotWith("Salut, ", "Yo les gens c lilian");
 	}
 }
 ?>
